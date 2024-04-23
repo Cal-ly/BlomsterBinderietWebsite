dcc70f901eb2bb1a2b9dd57750db73af5560c4c3
Cal-ly <cal-ly@outlook.dk>
2024-04-23 02:11:51 +0200

    Added Null check

--- HttpWebshopCookie/Models/OrderItem.cs
+++ HttpWebshopCookie/Models/OrderItem.cs
@@ -6,12 +6,12 @@ public class OrderItem
     public string Id { get; set; } = Guid.NewGuid().ToString();
 
     [ForeignKey("OrderId")]
-    public Order Order { get; set; }
-    public string OrderId { get; set; }
+    public Order? Order { get; set; }
+    public string? OrderId { get; set; }
 
     [ForeignKey("ProductId")]
-    public Product Product { get; set; }
-    public string ProductId { get; set; }
+    public Product? Product { get; set; }
+    public string? ProductId { get; set; }
 
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
